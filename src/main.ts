3946667dae4e8154531fe1c6ae4c7701d731c608
Dmitry Surdin <dsurdin@eyeconweb.com>
2025-04-12 15:44:32 +0400

    add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
   app.enableCors({});
   app.use(cookieParser());
   app.useGlobalFilters();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 
